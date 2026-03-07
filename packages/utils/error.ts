c5e9809e20d370d15879564085f8ff1cf55ad169
yangjiao <3273473023@qq.com>
2026-03-07 12:39:57 +0800

    utils:编写测试用例

--- packages/utils/error.ts
+++ packages/utils/error.ts
@@ -11,12 +11,16 @@ export function throwError(scope: string, msg: string) {
     throw new ErUIError(`[${scope}] ${msg}`)
 }
 
+export function createErUIError(scope: string, msg?: string) {
+    return new ErUIError(`[${scope}] ${msg}`)
+}
+
 export function debugWarn(error: Error): void;
 export function debugWarn(scope: string, msg: string): void
 
 export function debugWarn(scope: string | Error, msg?: string) {
     if (process.env.NODE_ENV !== "production") {
-        const err = isString(scope) ? new ErUIError(`[${scope}] accordion mode should only have one active item`) : scope
+        const err = isString(scope) ? createErUIError(scope, msg) : scope
         console.warn(err)
     }
 }
